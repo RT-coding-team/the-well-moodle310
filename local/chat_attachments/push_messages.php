a06eac534663ad710f3285b5510d86f7f2eb0845
Codemis <johnathan@pulos.com>
2021-03-11 11:38:15 -0800

    We are now sending the course details to the API.

--- local/chat_attachments/push_messages.php
+++ local/chat_attachments/push_messages.php
@@ -21,6 +21,7 @@ require_once($CFG->libdir . DIRECTORY_SEPARATOR . 'filelib.php');
 require_once(dirname(__FILE__) .DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'CurlUtility.php');
 
 $url = get_config('local_chat_attachments', 'messaging_url');
+$boxId = 1;
 echo 'Sending Requests to: ' . $url . '<br>';
 
 if ($url === '') {
@@ -30,6 +31,72 @@ if ($url === '') {
 
 $curl = new CurlUtility($url);
 
-echo 'Sending request to ' . $url . 'messageStatus/1<br>';
-$lastSync = $curl->makeRequest('messageStatus/1', 'GET');
+echo 'Sending request to ' . $url . 'messageStatus/' . $boxId . '<br>';
+$lastSync = $curl->makeRequest('messageStatus/' . $boxId, 'GET');
 echo 'Last Sync Time: ' . date('F j, Y H:i:s', $lastSync) . '(' . $lastSync . ')<br>';
+/**
+ * Create the course payload to send to the API
+ */
+$payload = [];
+$courses = get_courses();
+$studentRole = $DB->get_record('role', ['shortname' =>  'student']);
+$teacherRole = $DB->get_record('role', ['shortname' =>  'teacher']);
+$editingTeacherRole = $DB->get_record('role', ['shortname' =>  'editingteacher']);
+foreach ($courses as $course) {
+    $context = context_course::instance($course->id);
+    $data = [
+        'id'            =>  intval($course->id),
+        'course_name'   =>  $course->fullname,
+        'summary'       =>  $course->summary,
+        'created_on'    =>  intval($course->timecreated),
+        'updated_on'    =>  intval($course->timemodified),
+        'students'      =>  [],
+        'teachers'      =>  []
+    ];
+    $students = get_role_users($studentRole->id, $context);
+    foreach ($students as $student) {
+        $data['students'][] = [
+            'id'            =>  intval($student->id),
+            'username'      =>  $student->username,
+            'first_name'    =>  $student->firstname,
+            'last_name'     =>  $student->lastname,
+            'email'         =>  $student->email,
+            'last_accessed' =>  intval($student->lastaccess),
+            'language'      =>  $student->lang
+        ];
+    }
+    $teachers = get_role_users($teacherRole->id, $context);
+    foreach ($teachers as $teacher) {
+        $data['teachers'][] = [
+            'id'            =>  intval($teacher->id),
+            'username'      =>  $teacher->username,
+            'first_name'    =>  $teacher->firstname,
+            'last_name'     =>  $teacher->lastname,
+            'email'         =>  $teacher->email,
+            'last_accessed' =>  intval($teacher->lastaccess),
+            'language'      =>  $teacher->lang
+        ];
+    }
+    $editingTeachers = get_role_users($editingTeacherRole->id, $context);
+    foreach ($editingTeachers as $teacher) {
+        $data['teachers'][] = [
+            'id'            =>  intval($teacher->id),
+            'username'      =>  $teacher->username,
+            'first_name'    =>  $teacher->firstname,
+            'last_name'     =>  $teacher->lastname,
+            'email'         =>  $teacher->email,
+            'last_accessed' =>  intval($teacher->lastaccess),
+            'language'      =>  $teacher->lang
+        ];
+    }
+    $payload[] = $data;
+}
+echo 'Our Course Payload:<br><pre>';
+echo json_encode($payload, JSON_PRETTY_PRINT);
+echo '</pre><br>';
+/**
+ * Send the payload to the API
+ */
+echo 'Sending request to ' . $url . 'courseRosters/' . $boxId . '<br>';
+$curl->makeRequest('courseRosters/' . $boxId, 'POST', [], json_encode($payload));
+echo 'The response was ' . $curl->responseCode . '<br>';
