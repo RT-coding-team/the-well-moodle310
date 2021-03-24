30ce62de5f9a1067fa8e0c24db60dea377c03768
Codemis <johnathan@pulos.com>
2021-03-24 11:39:01 -0700

    We now handle progress bars.

--- tasks/assets/js/application.js
+++ tasks/assets/js/application.js
@@ -5,10 +5,25 @@
  */
 function pollServer() {
   $.get('/local/chat_attachments/report.json', function(data) {
+    if (data.progress !== null) {
+      $('#current-progress h5').text(data.progress.title);
+      var completed = (data.progress.current + data.progress.error);
+      var total = (completed/data.progress.total) * 100;
+      $('#current-progress .progress-bar').css('width', total + '%');
+      $('#current-progress .progress-bar').attr('aria-valuenow', total);
+      $('#current-progress .progress-bar').text(completed + ' / ' + data.progress.total);
+      $('#current-progress').removeClass('d-none');
+    } else {
+      setTimeout(function() {
+        $('#current-progress').addClass('d-none');
+      }, 1000);
+    }
     if (data.results.status === 'started') {
       $('button#sync').prop('disabled', true);
+      $('button#sync i').addClass('fa-spin');
     } else {
       $('button#sync').prop('disabled', false);
+      $('button#sync i').removeClass('fa-spin');
     }
     setTimeout(pollServer, 2000);
   });
@@ -18,6 +33,7 @@ $(function() {
     $('button#sync').on('click', function(event) {
       event.stopPropagation();
       $('button#sync').prop('disabled', true);
+      $('button#sync i').addClass('fa-spin');
       $.get('./sync.php').then(function() {});
       return false;
     });
