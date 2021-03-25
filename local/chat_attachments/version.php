0e751bdd8d3d41b65493447844b9e5f6d135a229
Codemis <johnathan@pulos.com>
2021-03-24 18:21:08 -0700

    Added an email setting.

--- local/chat_attachments/version.php
+++ local/chat_attachments/version.php
@@ -11,8 +11,8 @@ $plugin->requires = 2010112400;
  * Our version number
  *
  */
-$plugin->version = 10022;
+$plugin->version = 10023;
 $plugin->component = 'local_chat_attachments';
 $plugin->cron = 0;
-$plugin->release = '1.0.22 (Build: 10022)';
+$plugin->release = '1.0.23 (Build: 10023)';
 $plugin->maturity = MATURITY_STABLE;
